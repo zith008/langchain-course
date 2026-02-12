83c25ff43d5715dcf17c802a03f0f0099119e3a3
zith008 <fazithismail@gmail.com>
2026-02-12 19:08:15 +0800

    pydantic parsing example

--- main.py
+++ main.py
@@ -8,15 +8,26 @@ from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents import create_react_agent
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 tools = [TavilySearch()]
-llm = ChatOpenAI(model="gpt-4o")
+llm = ChatOpenAI(model="gpt-5")
 react_prompt = hub.pull("hwchase17/react")
+output_parser = PydanticOutputParser( pydantic_object = AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template = REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables = ["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions = output_parser.get_format_instructions())
 
 agent = create_react_agent(
     llm = llm,
     tools = tools,
-    prompt = react_prompt
+    prompt = react_prompt_with_format_instructions
 )
 
 agent_executor = AgentExecutor(
@@ -25,7 +36,7 @@ agent_executor = AgentExecutor(
     verbose = True,
     handle_parsing_errors=True 
 )
-
+ 
 chain = agent_executor
 
 def main():
